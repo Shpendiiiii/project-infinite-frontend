448547ded8f2b66dd2b9e65b7bf2a9ecd12cfa40
Shpendiiii <shpendaliu69@gmail.com>
2025-02-22 13:56:48 +0100

    Initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Ej pookie, routing works</p>
+<p class="text-red-800 text-[40px] text-center">Ej pookie, routing and tali wind works</p>
 
 <a href="/about">Go to about page</a>
